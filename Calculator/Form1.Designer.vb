d8d26eb6e8623d79578a6af70aeb17e04b378810
Pieter Erlanda Pribadi <pieteru.insekai@gmail.com>
2012-07-31 09:50:00 +0700

    Master Program
    This the calculator master program

---
+++ Calculator/Form1.Designer.vb
@@ -0,0 +1,301 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Txt1 = New System.Windows.Forms.TextBox()
+        Me.B1 = New System.Windows.Forms.Button()
+        Me.B2 = New System.Windows.Forms.Button()
+        Me.B3 = New System.Windows.Forms.Button()
+        Me.BDif = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.B4 = New System.Windows.Forms.Button()
+        Me.B5 = New System.Windows.Forms.Button()
+        Me.B6 = New System.Windows.Forms.Button()
+        Me.BMul = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.B7 = New System.Windows.Forms.Button()
+        Me.B8 = New System.Windows.Forms.Button()
+        Me.B9 = New System.Windows.Forms.Button()
+        Me.BMin = New System.Windows.Forms.Button()
+        Me.Button15 = New System.Windows.Forms.Button()
+        Me.B0 = New System.Windows.Forms.Button()
+        Me.B00 = New System.Windows.Forms.Button()
+        Me.Bdot = New System.Windows.Forms.Button()
+        Me.BPlu = New System.Windows.Forms.Button()
+        Me.Button20 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Txt1
+        '
+        Me.Txt1.Location = New System.Drawing.Point(6, 3)
+        Me.Txt1.Name = "Txt1"
+        Me.Txt1.Size = New System.Drawing.Size(199, 20)
+        Me.Txt1.TabIndex = 0
+        Me.Txt1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'B1
+        '
+        Me.B1.Location = New System.Drawing.Point(6, 29)
+        Me.B1.Name = "B1"
+        Me.B1.Size = New System.Drawing.Size(35, 23)
+        Me.B1.TabIndex = 1
+        Me.B1.Text = "1"
+        Me.B1.UseVisualStyleBackColor = True
+        '
+        'B2
+        '
+        Me.B2.Location = New System.Drawing.Point(47, 29)
+        Me.B2.Name = "B2"
+        Me.B2.Size = New System.Drawing.Size(35, 23)
+        Me.B2.TabIndex = 2
+        Me.B2.Text = "2"
+        Me.B2.UseVisualStyleBackColor = True
+        '
+        'B3
+        '
+        Me.B3.Location = New System.Drawing.Point(88, 29)
+        Me.B3.Name = "B3"
+        Me.B3.Size = New System.Drawing.Size(35, 23)
+        Me.B3.TabIndex = 3
+        Me.B3.Text = "3"
+        Me.B3.UseVisualStyleBackColor = True
+        '
+        'BDif
+        '
+        Me.BDif.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BDif.Location = New System.Drawing.Point(129, 29)
+        Me.BDif.Name = "BDif"
+        Me.BDif.Size = New System.Drawing.Size(35, 23)
+        Me.BDif.TabIndex = 4
+        Me.BDif.Text = "/"
+        Me.BDif.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button5.Location = New System.Drawing.Point(170, 29)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(35, 23)
+        Me.Button5.TabIndex = 5
+        Me.Button5.Text = "Mo"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'B4
+        '
+        Me.B4.Location = New System.Drawing.Point(6, 58)
+        Me.B4.Name = "B4"
+        Me.B4.Size = New System.Drawing.Size(35, 23)
+        Me.B4.TabIndex = 6
+        Me.B4.Text = "4"
+        Me.B4.UseVisualStyleBackColor = True
+        '
+        'B5
+        '
+        Me.B5.Location = New System.Drawing.Point(47, 58)
+        Me.B5.Name = "B5"
+        Me.B5.Size = New System.Drawing.Size(35, 23)
+        Me.B5.TabIndex = 7
+        Me.B5.Text = "5"
+        Me.B5.UseVisualStyleBackColor = True
+        '
+        'B6
+        '
+        Me.B6.Location = New System.Drawing.Point(88, 58)
+        Me.B6.Name = "B6"
+        Me.B6.Size = New System.Drawing.Size(35, 23)
+        Me.B6.TabIndex = 8
+        Me.B6.Text = "6"
+        Me.B6.UseVisualStyleBackColor = True
+        '
+        'BMul
+        '
+        Me.BMul.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BMul.Location = New System.Drawing.Point(129, 58)
+        Me.BMul.Name = "BMul"
+        Me.BMul.Size = New System.Drawing.Size(35, 23)
+        Me.BMul.TabIndex = 9
+        Me.BMul.Text = "*"
+        Me.BMul.UseVisualStyleBackColor = True
+        '
+        'Button10
+        '
+        Me.Button10.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button10.Location = New System.Drawing.Point(170, 58)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(35, 23)
+        Me.Button10.TabIndex = 10
+        Me.Button10.Text = "<-"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'B7
+        '
+        Me.B7.Location = New System.Drawing.Point(6, 87)
+        Me.B7.Name = "B7"
+        Me.B7.Size = New System.Drawing.Size(35, 23)
+        Me.B7.TabIndex = 11
+        Me.B7.Text = "7"
+        Me.B7.UseVisualStyleBackColor = True
+        '
+        'B8
+        '
+        Me.B8.Location = New System.Drawing.Point(47, 87)
+        Me.B8.Name = "B8"
+        Me.B8.Size = New System.Drawing.Size(35, 23)
+        Me.B8.TabIndex = 12
+        Me.B8.Text = "8"
+        Me.B8.UseVisualStyleBackColor = True
+        '
+        'B9
+        '
+        Me.B9.Location = New System.Drawing.Point(88, 87)
+        Me.B9.Name = "B9"
+        Me.B9.Size = New System.Drawing.Size(35, 23)
+        Me.B9.TabIndex = 13
+        Me.B9.Text = "9"
+        Me.B9.UseVisualStyleBackColor = True
+        '
+        'BMin
+        '
+        Me.BMin.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BMin.Location = New System.Drawing.Point(129, 87)
+        Me.BMin.Name = "BMin"
+        Me.BMin.Size = New System.Drawing.Size(35, 23)
+        Me.BMin.TabIndex = 14
+        Me.BMin.Text = "-"
+        Me.BMin.UseVisualStyleBackColor = True
+        '
+        'Button15
+        '
+        Me.Button15.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button15.Location = New System.Drawing.Point(170, 87)
+        Me.Button15.Name = "Button15"
+        Me.Button15.Size = New System.Drawing.Size(35, 23)
+        Me.Button15.TabIndex = 15
+        Me.Button15.Text = "C"
+        Me.Button15.UseVisualStyleBackColor = True
+        '
+        'B0
+        '
+        Me.B0.Location = New System.Drawing.Point(6, 116)
+        Me.B0.Name = "B0"
+        Me.B0.Size = New System.Drawing.Size(35, 23)
+        Me.B0.TabIndex = 16
+        Me.B0.Text = "0"
+        Me.B0.UseVisualStyleBackColor = True
+        '
+        'B00
+        '
+        Me.B00.Location = New System.Drawing.Point(47, 116)
+        Me.B00.Name = "B00"
+        Me.B00.Size = New System.Drawing.Size(35, 23)
+        Me.B00.TabIndex = 17
+        Me.B00.Text = "00"
+        Me.B00.UseVisualStyleBackColor = True
+        '
+        'Bdot
+        '
+        Me.Bdot.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Bdot.Location = New System.Drawing.Point(88, 116)
+        Me.Bdot.Name = "Bdot"
+        Me.Bdot.Size = New System.Drawing.Size(35, 23)
+        Me.Bdot.TabIndex = 18
+        Me.Bdot.Text = "."
+        Me.Bdot.UseVisualStyleBackColor = True
+        '
+        'BPlu
+        '
+        Me.BPlu.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BPlu.Location = New System.Drawing.Point(129, 116)
+        Me.BPlu.Name = "BPlu"
+        Me.BPlu.Size = New System.Drawing.Size(35, 23)
+        Me.BPlu.TabIndex = 19
+        Me.BPlu.Text = "+"
+        Me.BPlu.UseVisualStyleBackColor = True
+        '
+        'Button20
+        '
+        Me.Button20.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button20.Location = New System.Drawing.Point(170, 116)
+        Me.Button20.Name = "Button20"
+        Me.Button20.Size = New System.Drawing.Size(35, 23)
+        Me.Button20.TabIndex = 20
+        Me.Button20.Text = "="
+        Me.Button20.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(211, 145)
+        Me.Controls.Add(Me.Button20)
+        Me.Controls.Add(Me.BPlu)
+        Me.Controls.Add(Me.Bdot)
+        Me.Controls.Add(Me.B00)
+        Me.Controls.Add(Me.B0)
+        Me.Controls.Add(Me.Button15)
+        Me.Controls.Add(Me.BMin)
+        Me.Controls.Add(Me.B9)
+        Me.Controls.Add(Me.B8)
+        Me.Controls.Add(Me.B7)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.BMul)
+        Me.Controls.Add(Me.B6)
+        Me.Controls.Add(Me.B5)
+        Me.Controls.Add(Me.B4)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.BDif)
+        Me.Controls.Add(Me.B3)
+        Me.Controls.Add(Me.B2)
+        Me.Controls.Add(Me.B1)
+        Me.Controls.Add(Me.Txt1)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Txt1 As System.Windows.Forms.TextBox
+    Friend WithEvents B1 As System.Windows.Forms.Button
+    Friend WithEvents B2 As System.Windows.Forms.Button
+    Friend WithEvents B3 As System.Windows.Forms.Button
+    Friend WithEvents BDif As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents B4 As System.Windows.Forms.Button
+    Friend WithEvents B5 As System.Windows.Forms.Button
+    Friend WithEvents B6 As System.Windows.Forms.Button
+    Friend WithEvents BMul As System.Windows.Forms.Button
+    Friend WithEvents Button10 As System.Windows.Forms.Button
+    Friend WithEvents B7 As System.Windows.Forms.Button
+    Friend WithEvents B8 As System.Windows.Forms.Button
+    Friend WithEvents B9 As System.Windows.Forms.Button
+    Friend WithEvents BMin As System.Windows.Forms.Button
+    Friend WithEvents Button15 As System.Windows.Forms.Button
+    Friend WithEvents B0 As System.Windows.Forms.Button
+    Friend WithEvents B00 As System.Windows.Forms.Button
+    Friend WithEvents Bdot As System.Windows.Forms.Button
+    Friend WithEvents BPlu As System.Windows.Forms.Button
+    Friend WithEvents Button20 As System.Windows.Forms.Button
+
+End Class
---
+++ Calculator/Form1.vb
@@ -0,0 +1,158 @@
+﻿Public Class Form1
+
+    Private Property angka1 As Double
+    Private Property angka2 As Double
+    Private Property operasi As Char
+
+    Private Sub Inputan(x As Char, pindah As Boolean)
+        Txt1.Text = Txt1.Text + x
+        If pindah Then
+            angka1 = Val(Txt1.Text)
+        Else
+            angka2 = Val(Txt1.Text)
+        End If
+    End Sub
+
+    Private Sub B1_Click(sender As System.Object, e As System.EventArgs) Handles B1.Click
+        Txt1.Text = Txt1.Text + "1"
+        angka1 = Val(Txt1.Text)
+    End Sub
+
+    Private Sub B2_Click(sender As System.Object, e As System.EventArgs) Handles B2.Click
+        Txt1.Text = Txt1.Text + "2"
+        angka1 = Val(Txt1.Text)
+    End Sub
+
+    Private Sub B3_Click(sender As System.Object, e As System.EventArgs) Handles B3.Click
+        Txt1.Text = Txt1.Text + "3"
+        angka1 = Val(Txt1.Text)
+    End Sub
+
+    Private Sub B4_Click(sender As System.Object, e As System.EventArgs) Handles B4.Click
+        Txt1.Text = Txt1.Text + "4"
+        angka1 = Val(Txt1.Text)
+    End Sub
+
+    Private Sub B5_Click(sender As System.Object, e As System.EventArgs) Handles B5.Click
+        Txt1.Text = Txt1.Text + "5"
+        angka1 = Val(Txt1.Text)
+    End Sub
+
+    Private Sub B6_Click(sender As System.Object, e As System.EventArgs) Handles B6.Click
+        Txt1.Text = Txt1.Text + "6"
+        angka1 = Val(Txt1.Text)
+    End Sub
+
+    Private Sub B7_Click(sender As System.Object, e As System.EventArgs) Handles B7.Click
+        Txt1.Text = Txt1.Text + "7"
+        angka1 = Val(Txt1.Text)
+    End Sub
+
+    Private Sub B8_Click(sender As System.Object, e As System.EventArgs) Handles B8.Click
+        Txt1.Text = Txt1.Text + "8"
+        angka1 = Val(Txt1.Text)
+    End Sub
+
+    Private Sub B9_Click(sender As System.Object, e As System.EventArgs) Handles B9.Click
+        Txt1.Text = Txt1.Text + "9"
+        angka1 = Val(Txt1.Text)
+    End Sub
+
+    Private Sub B0_Click(sender As System.Object, e As System.EventArgs) Handles B0.Click
+        Txt1.Text = Txt1.Text + "0"
+        angka1 = Val(Txt1.Text)
+    End Sub
+
+    Private Sub B00_Click(sender As System.Object, e As System.EventArgs) Handles B00.Click
+        Txt1.Text = Txt1.Text + "00"
+        angka1 = Val(Txt1.Text)
+    End Sub
+
+    Private Sub Bdot_Click(sender As System.Object, e As System.EventArgs) Handles Bdot.Click
+        Dim at As Integer
+        If Txt1.Text <> "" Then
+            at = Txt1.Text.LastIndexOfAny(".")
+            If at = -1 Then
+                Txt1.Text = Txt1.Text + "."
+            End If
+        Else
+            Txt1.Text = Txt1.Text + "0."
+        End If
+        angka1 = Val(Txt1.Text)
+        
+    End Sub
+
+    Private Sub BDif_Click(sender As System.Object, e As System.EventArgs) Handles BDif.Click
+        Txt1.Text = ""
+        If angka1 <> 0 And angka2 = 0 Then
+            angka2 = angka1
+            angka1 = 0
+        End If
+        operasi = "/"
+    End Sub
+
+    Private Sub Button20_Click(sender As System.Object, e As System.EventArgs) Handles Button20.Click
+        Select Case operasi
+            Case "/" : angka2 = angka2 / angka1
+            Case "*" : angka2 = angka2 * angka1
+            Case "-" : angka2 = angka2 - angka1
+            Case "+" : angka2 = angka2 + angka1
+            Case "Mo" : angka2 = angka2 Mod angka1
+            Case Else : angka2 = 0
+        End Select
+        angka1 = 0
+        Txt1.Text = angka2
+    End Sub
+
+    Private Sub BMul_Click(sender As System.Object, e As System.EventArgs) Handles BMul.Click
+        Txt1.Text = ""
+        If angka1 <> 0 And angka2 = 0 Then
+            angka2 = angka1
+            angka1 = 0
+        End If
+        operasi = "*"
+    End Sub
+
+    Private Sub BMin_Click(sender As System.Object, e As System.EventArgs) Handles BMin.Click
+        Txt1.Text = ""
+        If angka1 <> 0 And angka2 = 0 Then
+            angka2 = angka1
+            angka1 = 0
+        End If
+        operasi = "-"
+    End Sub
+
+    Private Sub BPlu_Click(sender As System.Object, e As System.EventArgs) Handles BPlu.Click
+        Txt1.Text = ""
+        If angka1 <> 0 And angka2 = 0 Then
+            angka2 = angka1
+            angka1 = 0
+        End If
+        operasi = "+"
+    End Sub
+
+    Private Sub Button5_Click(sender As System.Object, e As System.EventArgs) Handles Button5.Click
+        Txt1.Text = ""
+        If angka1 <> 0 And angka2 = 0 Then
+            angka2 = angka1
+            angka1 = 0
+        End If
+        operasi = "Mo"
+    End Sub
+
+    Private Sub Button15_Click(sender As System.Object, e As System.EventArgs) Handles Button15.Click
+        Txt1.Text = ""
+        angka1 = 0
+        angka2 = 0
+    End Sub
+
+    Private Sub Button10_Click(sender As System.Object, e As System.EventArgs) Handles Button10.Click
+        If Txt1.Text <> "" Then
+            Txt1.Text = Txt1.Text.Substring(0, Txt1.Text.Length - 1)
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Txt1.Text = ""
+    End Sub
+End Class
